fbffbd1da35275eb90055b5d29612d16b45c9260
Jeremiah <jb@jbhometraining.com>
2020-06-13 13:18:53 -0500

    pagination and function tests

--- static/script.js
+++ static/script.js
@@ -1,33 +1,86 @@
-paginate();
-function paginate() {
-  let totalPages;
-  let pageStart;
-  let pageEnd;
-  const resultTotal = $('#result-length').val();
-  let pageNum = $('#page-num').val();
-  let remainder = resultTotal % 100;
-  if (resultTotal <= 100) {
-      totalPages = 1;
-  }
-  else if (remainder > 0) {
-      totalPages = parseInt(resultTotal / 100) + 1;
-  }
-  else {
-      totalPages = parseInt(resultTotal / 100);
-  }
-  
-  if (totalPages <= 10) {
-      pageStart = 1;
-      pageEnd = totalPages;
-  }
-  else {
-      pageStart = Math.floor(pageNum / 10) * 10 + 1;
-      pageEnd = (pageStart + 10) <= totalPages ? (pageStart + 10) : totalPages;
+const pageNum = parseInt($("#page-num").val());
+const resultTotal = parseInt($("#result-length").val());
+
+paginate(pageNum, resultTotal);
+
+// displays page links on results page
+function paginate(pageNum, resultTotal) {
+  const totalPages = getTotalPages(resultTotal);
+  const start = getPageStart(totalPages, pageNum);
+  const end = getPageEnd(totalPages, start);
+  const next = getNextPageSet(totalPages, end);
+  const previous = getPreviousPageSet(next);
+
+  $('#next').text('Next').attr("href", `/planets/results/${next}`);
+
+  if (pageNum >= 10) {
+    $('#previous').text('Previous').attr("href", `/planets/results/${previous}`);
   }
 
-  for (let i = pageStart; i <= pageEnd; i++) {
-      $('#pages').append($(`<div class="page-buttons">
-                <a href="/planets/results/${i}" id="${i}">${i}</a></div>`));
+  if (!end) {
+    end = 2;
+  }
+
+  for (let i = start; i <= end; i++) {
+    $("#pages").append(
+      $(`<div class="page-buttons">
+                <a href="/planets/results/${i}" id="${i}">${i}</a></div>`)
+    );
   }
   $(`#${pageNum}`).removeAttr("href");
+}
+
+// returns total number of pages (100 results each) depending on number of returned results
+function getTotalPages(resultTotal) {
+  let remainder = resultTotal % 100;
+  if (resultTotal <= 100) {
+    return 1;
+  } else if (remainder > 0) {
+    return Math.floor(resultTotal / 100) + 1;
+  } else {
+    return resultTotal / 100;
+  }
+}
+
+// returns first page link of page set
+function getPageStart(totalPages, pageNum) {
+  if (totalPages <= 9 || pageNum <= 9) {
+    return 1;
+  }
+  else {
+    return Math.floor(pageNum / 10) * 10;
+  }
+}
+
+// returns last page link of page set
+function getPageEnd(totalPages, pageStart) {
+  if (totalPages <= 9 && totalPages > 1) {
+    return totalPages;
+  }  
+  else if (pageStart === 1) {
+    return 9;
+  }
+  else {
+    return pageStart + 9 <= totalPages ? pageStart + 9 : totalPages;
+  }
+}
+
+// returns next set of 10 pages
+function getNextPageSet(totalPages, pageEnd) {
+  const nextTen = pageEnd + 1;
+  if (totalPages >= 9 && pageEnd < totalPages) {   
+    if (totalPages < nextTen)
+      return totalPages;
+    return nextTen;
+  }
+}
+
+// returns previous set of 10 pages
+function getPreviousPageSet(nextSet) {
+  if (nextSet == 20) {
+    return 1;
+  }
+  else if (nextSet > 20) {
+    return nextSet - 20;
+  }
 }
--- static/test_script.js
+++ static/test_script.js
@@ -1,9 +1,40 @@
 describe ("Testing functions for exoplanet app", () => {
 
-    it ("should submit new favorite planet to user list and receive response with addPlanet", async function() {
-        resp = await addPlanet("testuser", 1, "11 Com b");
+    // it ("should submit new favorite planet to user list and receive response with addPlanet", async function() {
+    //     resp = await addPlanet("testuser", 1, "11 Com b");
 
-        expect(resp.data.new_favorite.list).toEqual("testplanets");
-        expect(resp.data.new_favorite.planet).toEqual("11 Com b");
+    //     expect(resp.data.new_favorite.list).toEqual("testplanets");
+    //     expect(resp.data.new_favorite.planet).toEqual("11 Com b");
+    // })
+    
+    it ("should return total number of result pages with getTotalPages", () => {
+        expect(getTotalPages(4164)).toEqual(42);
+        expect(getTotalPages(9)).toEqual(1);
+        expect(getTotalPages(101)).toEqual(2);
+    })
+
+    it ("should return first page link of set on page with getPageStart", () => {
+        expect(getPageStart(4164, 8)).toEqual(1);
+        expect(getPageStart(9, 1)).toEqual(1);
+        expect(getPageStart(1000,11)).toEqual(10);
+    })
+    
+    it ("should return last page link of set on page with getPageEnd", () => {
+        expect(getPageEnd(4164, 1)).toEqual(9);
+        expect(getPageEnd(4164, 10)).toEqual(19);
+        expect(getPageEnd(9,1)).toEqual(9);
+    })
+
+    it ("should return start of next set of 10 pages with getNextPageSet", () => {
+        expect(getNextPageSet(40, 29)).toEqual(30);
+        expect(getNextPageSet(9, 9)).toBeUndefined();
+        expect(getNextPageSet(12, 9)).toEqual(10);
+        expect(getNextPageSet(40, 39)).toBe(40);
+    })
+    
+    it ("should return start of previous set of 10 pages with getPreviousPageSet", () => {
+        expect(getPreviousPageSet(20)).toEqual(1);        
+        expect(getPreviousPageSet(10)).toBeUndefined();        
+        expect(getPreviousPageSet(40)).toEqual(20);        
     })
 })
